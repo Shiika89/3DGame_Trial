b791ec147d7c4d413c5b550f00cd7ae9bafef433
shiika89 <kazuki_plant@yahoo.co.jp>
2021-05-19 12:40:14 +0900

    ゾンビが一定範囲内に入ったらプレイヤーを追いかけて出たら止まるように変更

--- Assets/EnemyMove.cs
+++ Assets/EnemyMove.cs
@@ -7,29 +7,41 @@ using UnityEngine.AI;
 
 public class EnemyMove : MonoBehaviour
 {
-    [SerializeField] GameObject m_Player;
+    //向かう相手を決めるための変数
+    [SerializeField] Transform m_target;
     [SerializeField] Animator m_anim;
+    //動くスピード
+    [SerializeField] float m_moveSpeed = 0f;
+    //相手が検知範囲に入ったどうか調べるための変数
     public bool m_inArea = false;
-    //Rigidbody m_rb;
+    Rigidbody m_rb;
     NavMeshAgent m_navMeshAgent;
 
     // Start is called before the first frame update
     void Start()
     {
         m_navMeshAgent = GetComponent<NavMeshAgent>();
-        //m_rb = GetComponent<Rigidbody>();
+        m_rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        //検知エリアに入ったら動いて出たら止まる
         if (m_inArea == true)
         {
-            m_navMeshAgent.destination = m_Player.transform.position;
+            //m_navMeshAgent.destination = m_target.transform.position;
+            //m_anim.SetBool("Inarea", true);
+            Move();
             Animation();
         }
         else
         {
+            //Speedを０にする
+            Vector3 velocity = m_rb.velocity;
+            velocity.y = 0f;
+            m_anim.SetFloat("Speed", 0);
+
             return;
         }
     }
@@ -38,7 +50,8 @@ public class EnemyMove : MonoBehaviour
     {
         if (m_anim)
         {
-            Vector3 velocity = m_navMeshAgent.velocity;
+            //Speedにm_rbの速度を入れる
+            Vector3 velocity = m_rb.velocity;
             velocity.y = 0f;
             m_anim.SetFloat("Speed", velocity.magnitude);
         }
@@ -60,5 +73,19 @@ public class EnemyMove : MonoBehaviour
         {
             m_inArea = false;
         }
+    }
+
+    void Move()
+    {
+        //自身をtargetの方向に向かせる
+        Vector3 targetPos = m_target.position;
+        targetPos.y = transform.position.y;
+        transform.LookAt(targetPos);
+
+        //向いてる方向に進ませる
+        Vector3 velo = this.transform.forward * m_moveSpeed;
+        velo.y = m_rb.velocity.y;
+        m_rb.velocity = velo;
+        transform.position = transform.position + velo * Time.deltaTime;
     }
 }
