1f6f95ecca2788059f6780fbacf45027660c929f
shiika89 <kazuki_plant@yahoo.co.jp>
2021-06-21 17:51:41 +0900

    敵にHPバーを追加

--- Assets/Script/EnemyDesroy.cs
+++ Assets/Script/EnemyDesroy.cs
@@ -1,12 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EnemyDesroy : MonoBehaviour
 {
     [SerializeField] public int m_life = 100;
     [SerializeField] GameObject m_enemyDie;
-    [SerializeField] int currentLife;
+    [SerializeField] int m_currentLife;
+    [SerializeField] private GameObject m_HPUI;
+    private Slider m_slinder;
 
     int Damage = 10;
 
@@ -14,20 +17,21 @@ public class EnemyDesroy : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        currentLife = m_life;
+        m_slinder = m_HPUI.transform.Find("HPBar").GetComponent<Slider>();
+        m_currentLife = m_life;
+        m_slinder.maxValue = m_life;
+        m_slinder.value = m_currentLife;
     }
-
-    // Update is called once per frame
-    
 
     public void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "Attack")
         {
-           currentLife -= Damage;
+            m_currentLife -= Damage;
+            m_slinder.value -= Damage;
 
 
-            if (currentLife <= 0)
+            if (m_currentLife <= 0)
             {
                 Debug.Log("エネミー死亡");
                 GameObject death = Instantiate(m_enemyDie);
