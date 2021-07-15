990350d5f63dd5fa3f25d2b7266540619ecfad78
shiika89 <kazuki_plant@yahoo.co.jp>
2021-07-15 19:42:39 +0900

    Spaceキーで装備画面を表示できるように修正、GameManagerを追加

---
+++ Assets/Script/Gamemanager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gamemanager : MonoBehaviour
+{
+    [SerializeField] GameObject m_EquipmentUI;
+    private bool m_UIflag = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && m_UIflag == false)
+        {
+            m_EquipmentUI.SetActive(true);
+            m_UIflag = true;
+        }
+        else if (Input.GetKeyDown(KeyCode.Space) && m_UIflag == true)
+        {
+            m_EquipmentUI.SetActive(false);
+            m_UIflag = false;
+        }
+    }
+}
