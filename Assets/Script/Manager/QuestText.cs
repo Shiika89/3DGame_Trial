162b52adb963eaae758551d0c29787f0e1602dc9
shiika89 <kazuki_plant@yahoo.co.jp>
2021-09-24 15:53:08 +0900

    ミニマップの追加、目標のテキスト追加

---
+++ Assets/Script/Manager/QuestText.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class QuestText : MonoBehaviour
+{
+    [SerializeField] Text m_questText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!Gamemanager.m_key)
+        {
+            m_questText.text = "鍵を探そう！";
+        }
+        else
+        {
+            m_questText.text = "階段を目指そう！";
+        }
+    }
+}
