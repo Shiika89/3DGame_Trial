d029f9e063f43337c34a2653d4d3f925638a3626
shiika89 <kazuki_plant@yahoo.co.jp>
2022-05-24 20:05:04 +0900

    スクリプトの修正

--- Assets/Script/CameraShake.cs
+++ Assets/Script/CameraShake.cs
@@ -6,32 +6,33 @@ public class CameraShake : MonoBehaviour
 {
     [SerializeField] float m_shakeRange = 0.2f;
     [SerializeField] float m_shakeTime = 0.2f;
+
     bool m_isShake;
     WaitForFixedUpdate waitFixedUpdate = new WaitForFixedUpdate();
+
     public void StartShake()
     {
-        if (m_isShake)
-        {
-            return;
-        }
+        if (m_isShake) return;
+
         StartCoroutine(Shake());
     }
 
     IEnumerator Shake()
     {
         m_isShake = true;
+
         var startPos = transform.localPosition;
         var shakePos = startPos;
-        int p = Random.Range(0, 2);
-        int p2 = Random.Range(0, 2);
 
-        shakePos.x += m_shakeRange * (1 + (-2 * p));
-        shakePos.y += m_shakeRange * (1 + (-2 * p2));
+        int range1 = Random.Range(0, 2);
+        int range2 = Random.Range(0, 2);
+
+        shakePos.x += m_shakeRange * (1 + (-2 * range1));
+        shakePos.y += m_shakeRange * (1 + (-2 * range2));
 
         transform.localPosition = shakePos;
         var timer = m_shakeTime;
 
-        Debug.Log(shakePos);
         while (timer > 0)
         {
             yield return waitFixedUpdate;
